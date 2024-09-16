645d3db97dac52857be0d27ec2c1fd1f68eae32a
YoranT <108663797+YoranT@users.noreply.github.com>
2024-09-16 11:57:07 +0200

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,26 @@
+import asyncio
+import logging
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Evva Airkey from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    # Set up the sensors
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
